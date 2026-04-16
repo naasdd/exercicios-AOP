ee222db332685332e216fb48c577059a2f340173
José Gabriel Santos Gomes <josegabrielg2014@gmail.com>
2026-04-16 02:25:49 -0300

    [Aula 4] Exercicio 2 pronto

--- aula04/exercicio1.asm
+++ aula04/exercicio1.asm
@@ -24,19 +24,19 @@ ecall
 add t2, zero, a0
 
 
-loop:
-beq t1, t2, fimLoop # Verifica se roda o loop ou nao
+mdc_while:
+	beq t1, t2, mdc_fim # Verifica se roda o loop ou nao
 
-addi t3, t1, 1
-blt t3, t2, else  # IF
-sub t1, t1, t2
-jal loop
+	addi t3, t1, 1
+	blt t3, t2, else  # if (a > b) virou if (a <= b)
+		sub t1, t1, t2
+		jal mdc_while
 
-else:
-sub t2, t2, t1
-jal loop
+	else:
+	sub t2, t2, t1
+	jal mdc_while
 
-fimLoop:
+mdc_fim:
 
 
 # Exibir resultado
@@ -46,4 +46,7 @@ ecall
 
 addi a7, zero, 1
 add a0, zero, t1
+ecall
+
+addi a7, zero, 10
 ecall
